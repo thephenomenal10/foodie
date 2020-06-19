eb1e265b15bb0b41dc2093a808b57d38c8cae309
SaiBhavadeesh <saibhavadeesh@gmail.com>
2020-06-19 12:28:44 +0530

    bug fix

--- lib/vendors/screens/forgotPass.dart
+++ lib/vendors/screens/forgotPass.dart
@@ -10,6 +10,7 @@ class ForgotPass extends StatefulWidget {
 
 class _ForgotPassState extends State<ForgotPass> {
   TextEditingController emailController = new TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,110 +21,117 @@ class _ForgotPassState extends State<ForgotPass> {
           Expanded(
             child: SizedBox(),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.3,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 10.0),
-                Row(
-                  children: <Widget>[
-                    Flexible(
-                      flex: 2,
-                      child: TextFormField(
-                          controller: emailController,
-                          validator: (value) {
-                            Pattern pattern =
-                                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+          Form(
+            key: _formKey,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 10.0),
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 2,
+                        child: TextFormField(
+                            controller: emailController,
+                            validator: (value) {
+                              Pattern pattern =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
-                            RegExp regex = new RegExp(pattern);
-                            if (!regex.hasMatch(value)) {
-                              return 'Please enter a valid email id';
-                            }
-                            return null;
-                          },
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontSize: 12),
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            fillColor: Color(0xFF00B712),
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.all(0.0),
-                              child: Icon(
-                                Icons.email,
-                                color: Colors.white,
+                              RegExp regex = new RegExp(pattern);
+                              if (!regex.hasMatch(value)) {
+                                return 'Please enter a valid email id';
+                              }
+                              return null;
+                            },
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 12),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              fillColor: Color(0xFF00B712),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
                               ),
-                            ),
-                            hintText: 'Enter your email',
-                            hintStyle: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          )),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                  child: Container(
-                    width: 150,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(6.0),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Color(0xFF6078ea).withOpacity(.3),
-                              offset: Offset(0.0, 8.0),
-                              blurRadius: 8.0)
-                        ]),
-                    child: Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () async {
-                          await resetPassword(emailController.text);
-                          Navigator.of(context).pop();
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: Text('Alert'),
-                              content: Text(
-                                  'A password reset mail is sent to your Mail-Id'),
-                              actions: <Widget>[
-                                FlatButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('Login'),
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                        child: Center(
-                          child: Text("Reset",
-                              style: TextStyle(
+                              prefixIcon: Padding(
+                                padding: EdgeInsets.all(0.0),
+                                child: Icon(
+                                  Icons.email,
                                   color: Colors.white,
-                                  fontSize: 18,
-                                  letterSpacing: 1.0)),
+                                ),
+                              ),
+                              hintText: 'Enter your email',
+                              hintStyle: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    child: Container(
+                      width: 150,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(6.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xFF6078ea).withOpacity(.3),
+                                offset: Offset(0.0, 8.0),
+                                blurRadius: 8.0)
+                          ]),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () async {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+                              await resetPassword(emailController.text);
+                              Navigator.of(context).pop();
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text('Alert'),
+                                  content: Text(
+                                      'A password reset mail is sent to your Mail-Id'),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('Login'),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }
+                          },
+                          child: Center(
+                            child: Text("Reset",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    letterSpacing: 1.0)),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
